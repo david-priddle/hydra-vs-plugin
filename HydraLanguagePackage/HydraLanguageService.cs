63b1526792ed4a37081b9c48a11154a118859c3e
David Priddle <david.priddle@red-gate.com>
2016-02-02 16:27:42 +0000

    Put in a new VSPackage

---
+++ HydraLanguagePackage/HydraLanguageService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.Package;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace HydraLanguagePackage
+{
+    class HydraLanguageService : LanguageService
+    {
+        private LanguagePreferences m_LanguagePreferences;
+
+        public override LanguagePreferences GetLanguagePreferences()
+        {
+            if (m_LanguagePreferences == null)
+            {
+                m_LanguagePreferences = new LanguagePreferences(Site, typeof (HydraLanguageService).GUID, Name);
+
+                m_LanguagePreferences.Init();
+            }
+
+            return m_LanguagePreferences;
+        }
+
+        public override IScanner GetScanner(IVsTextLines buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override AuthoringScope ParseSource(ParseRequest req)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetFormatFilterList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Name { get; }
+    }
+}
---
+++ HydraLanguagePackage/HydraScanner.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.Package;
+
+namespace HydraLanguagePackage
+{
+    class HydraScanner : IScanner
+    {
+        public void SetSource(string source, int offset)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ScanTokenAndProvideInfoAboutIt(TokenInfo tokenInfo, ref int state)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
